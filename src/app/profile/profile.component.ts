64ab14095ad61d75c7fd2e49994d697edf9ac991
Irimurielle <irimurielle@gmail.com>
2020-11-26 10:28:38 +0200

    added corresponding codes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../user'
+import { SearchService } from '../search.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers:[SearchService]
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user:User;
 
-  ngOnInit(): void {
+  gitusername="";
+  constructor (private searchservice:SearchService) {
+    
+  }
+  SearchName(){
+    this.searchservice.getUser(this.gitusername)
   }
 
+  ngOnInit(){
+    this.searchservice.getUser('irimurielle')
+    this.user=this.searchservice.user
+  }
 }
