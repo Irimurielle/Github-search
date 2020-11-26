f584259859cdd491d87d83a1bcb377c5da0d9dcf
Irimurielle <irimurielle@gmail.com>
2020-11-26 10:29:09 +0200

    added code

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Repos } from '../repos';
+import { SearchService } from '../search.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.css']
+  styleUrls: ['./repository.component.css'],
+  providers:[SearchService]
 })
 export class RepositoryComponent implements OnInit {
 
-  constructor() { }
+  repos:Repos[];
+
+  gitusername="";
+  constructor(private searchservice:SearchService) { }
+
+  SearchRepo(){
+    this.searchservice.getRepos(this.gitusername)
+  }
 
   ngOnInit(): void {
+    this.searchservice.getRepos('irimurielle')
+    this.repos=this.searchservice.repos
+    console.log(this.repos)
   }
 
 }
+
